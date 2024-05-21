96ab9d840680d0e53e7236015fe382aa858510f2
anjali-singh8479 <shyanj05@gmail.com>
2024-05-21 10:47:33 +0530

    created home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const posts = [
+    {
+      id: 1,
+      img: "https://images4.alphacoders.com/271/271158.jpg",
+      title: "post",
+      content: "this is the content",
+    },
+    {
+      id: 2,
+      img: "https://images4.alphacoders.com/271/271158.jpg",
+      title: "post",
+      content: "this is the content",
+    },
+    {
+      id: 2,
+      img: "https://images4.alphacoders.com/271/271158.jpg",
+      title: "post",
+      content: "this is the content",
+    },
+    {
+      id: 2,
+      img: "https://images4.alphacoders.com/271/271158.jpg",
+      title: "post",
+      content: "this is the content",
+    },
+  ];
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="posts">
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={post.img} alt=""></img>
+            </div>
+            <div className="content">
+              <Link>
+                <h1 className="title">{post.title}</h1>
+              </Link>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptate, doloribus? Odio, vel vitae? Consectetur vel ratione
+                asperiores earum minima excepturi praesentium, sint adipisci
+                laborum non dolor ducimus. Necessitatibus, doloribus reiciendis.
+              </p>
+              <buttona className="button-readmore">Read More</buttona>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
